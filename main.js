74f9878678c373706f2a381b1bca5882c2b25ae4
Asami Jonsson <momoji303@gmail.com>
2021-11-24 23:13:35 +0100

    Fixed code

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// byta färg på sidan (mörk backgrund med ljus text, ljus backgrund med mörk text)
+// Byta färg på sidan (mörk bakgrundsfärg med ljus text, ljus bakgrundsfärg med mörk text)
 let darkMode = document.querySelector("#change-background");
-let questionOne = document.querySelector("#question1X3");
 let allQuestion = document.querySelector("#quiz");
 darkMode.addEventListener("click", () => {
   let body = document.querySelector("body");
@@ -12,88 +11,61 @@ darkMode.addEventListener("click", () => {
     body.style.color = "white";
   }
 });
-// body.style.background = "#131E3A";
-// body.style.color ="#D9DDDC";
 
+//Längst ned i quizet ska det finnas en knapp med texten “Get results!”
 let button = document.createElement("button");
 button.innerHTML = "Get reslut!";
 allQuestion.appendChild(button);
 
 button.addEventListener("click", () => {
   let score = 0;
-
   let answers = document.querySelectorAll("#answer");
 
-  for (let x = 0; x < answers.length; x++) {
-    let correctAnswers = answers[x];
+  //Tre frågor (tre svarsalternativ)
+  for (let i = 0; i < answers.length; i++) {
+    let correctAnswers = answers[i];
     if (correctAnswers.value == "correct" && correctAnswers.checked) {
       score++;
     }
   }
 
-  let q7Answers = document.querySelectorAll("#question7");
+  //En fråga (besvaras med checkboxar, minst fyra svarsalternativ)
+  //Hämta alla frågorna
+  let lastQuestions = document.querySelectorAll("#question7");
+  let finalAnswer = 0;
+  let allCheckbox = 0;
 
-  for (let x = 0; x < q7Answers.length; x++) {
-    let correctAnswers7 = q7Answers[x];
-    if (
-      correctAnswers7.value === "correct1" &&
-      correctAnswers7.value === "correct2" &&
-      correctAnswers7.value === "correct3" &&
-      correctAnswers7.checked
-    ) {
-      score++;
+  //Loopa igenom samtliga frågor från Q7
+  for (let i = 0; i < lastQuestions.length; i++) {
+    //Räknar alla checkboxar som har value="true" och som är markerade
+    if (lastQuestions[i].checked && lastQuestions[i].value == "true") {
+      finalAnswer++;
+    }
+    //Räknar alla checkboxar som är markerade
+    if (lastQuestions[i].checked) {
+      allCheckbox++;
     }
   }
 
+  //Bara "finalAnswer" X3 får 1 poäng
+  if (finalAnswer === 3 && allCheckbox < 4 && allCheckbox > 2) {
+    score++;
+  }
+  //Resultatet ska skrivas ut i DOM:en
   let totalScore = document.querySelector("#message");
-  totalScore.innerHTML = score;
+  totalScore.innerHTML = `Your score is ${score}`;
+
+  //Över hälften rätt =>orange färg text
+  //Alla rätt =>grön färg text
+  //Annars =>röd färg text
+
+  if (score > 3) {
+    totalScore.style.color = "orange";
+  }
+  if (score < 3) {
+    totalScore.style.color = "red";
+  }
+  if (score === 7) {
+    totalScore.style.color = "green";
+  }
 });
-// function quiz1(){
-//   let q1 = document.querySelector("#question1");
-//   if()
-//   if(q1.value == "correct"){
-//     score++;
-//       }
-//  }
-
-// function quiz2(){
-//   let q2 = document.querySelector("#question2");
-//   if(q2.answer.value == 'true'){
-//     score++;
-//   }
-// }
-
-// function quiz3(){
-//   let q3 = document.querySelector("#question3");
-//   if(q3.answer.value == 'false'){
-//     score++;
-//   }
-// }
-
-// let button = document.createElement("button");
-// button.innerHTML = "Get reslut!";
-// allQuestion.appendChild(button);
-
-// // tre frågor (sant eller falsk)
-// let answer1 = () => {
-//   if (question1Btn === ture) {
-//     window.alert("correct!!");
-//   }
-// };
-
-// button.addEventListener("click", () => {
-//   let totalScore = document.querySelector("#message");
-//   totalScore.innerHTML = score;
-// });
-
-//tre frågor (tre svarsalternativ)
-
-//en fråga (besvaras med checkboxar, minst fyra svarsalternativ)
-
-//Längst ned i quizet ska det finnas en knapp med texten “Get results!”
-
-// resultat ska skrivas ut i DOM:en
-
-//över hälften rätt =>orange färg text
-//alla rätt =>grön färg text
-//annars =>röd färg text
